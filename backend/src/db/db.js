a2d926963d9312768b36964a94e1dd89032f28c0
sandesh-200 <sandesh.dh@technimus.com>
2026-05-22 21:01:14 +0545

    prod(system):setup config files and env vars for deployemnt

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,13 +1,14 @@
 import mongoose, { mongo } from "mongoose";
 
-function connectDB(){
-    mongoose.connect(process.env.MONGODB_URI)
-    .then(()=>{
-        console.log("MONGODB CONNECTED")
+function connectDB() {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("MONGODB CONNECTED");
     })
-    .catch((error)=>{
-        console.log("MONGODB connection error: ",error)
-    })
+    .catch((error) => {
+      console.log("MONGODB connection error: ", error);
+    });
 }
 
-export default connectDB
+export default connectDB;
